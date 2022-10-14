ba9b55cab76523a19f79fdef698b0f0d37613904
김동희 <dongh94@naver.com>
2022-10-14 16:53:14 +0900

    뱀풀었따.

--- 1014/3190_뱀2.py
+++ 1014/3190_뱀2.py
@@ -7,13 +7,13 @@ dr = [0, 1, 0, -1]
 dc = [1, 0, -1, 0]
 
 N = int(input())
-arr = [[0]*(N) for _ in range(N)]
+arr = [[0]*(N+1) for _ in range(N+1)]
 
 # 사과의 위치
 K = int(input())
 for _ in range(K):
     r, c = map(int, input().split())
-    arr[r-1][c-1] = -1
+    arr[r][c] = -1
 # 방향 정보
 orders = dict()
 L = int(input())
@@ -31,7 +31,7 @@ def Direct(d, k):
         return d
 
 # 명령어
-x = y = 0
+x = y = 1
 arr[x][y] = 1
 Q = deque([(x,y)])
 d = 0
@@ -42,23 +42,23 @@ while Q:
     x += dr[d]
     y += dc[d]
     move += 1
-    #벽 안 이면서~!
-    if 0 <= x < N and 0 <= y < N and arr[x][y] != 1:
-        #사과면 먹고 1로 남겨
-        if arr[x][y] == -1:
-            arr[x][y] = 1
-            Q.append((x,y))
-            # 방향 바꿔
-            if orders.get(move):
-                d = Direct(d, orders[move])
-        #사과없으면 앞으로 전진 1, 꼬리 0 으로 바꿔!
-        elif arr[x][y] == 0:
-            arr[x][y] = 1
-            Q.append((x,y))
-            tx, ty = Q.popleft()
-            arr[tx][ty] = 0
-            if orders.get(move):
-                d = Direct(d, orders[move])
+    #벽 일단 아웃
+    if 1 > x or x >= N+1 or 1 > y or y >= N+1: break;
+    #사과면 먹고 1로 남겨
+    if arr[x][y] == -1:
+        arr[x][y] = 1
+        Q.append((x,y))
+        # 방향 바꿔
+        if orders.get(move):
+            d = Direct(d, orders[move])
+    #사과없으면 앞으로 전진 1, 꼬리 0 으로 바꿔!
+    elif arr[x][y] == 0:
+        arr[x][y] = 1
+        Q.append((x,y))
+        tx, ty = Q.popleft()
+        arr[tx][ty] = 0
+        if orders.get(move):
+            d = Direct(d, orders[move])
     else:
         break
 print(move)
